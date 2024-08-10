87d5528bdfd73c48cfb805d79f4e43c9a45809da
lmeetr@qq.com <>
2024-08-10 21:23:13 +0800

    1. add comments 2. save the q table

--- Q-learning/environment.py
+++ Q-learning/environment.py
@@ -16,32 +16,42 @@ class Env(tk.Tk):
         self.action_space = ['u', 'd', 'l', 'r']
         self.n_actions = len(self.action_space)
         self.title('Q Learning')
+        # 创建窗口
         self.geometry('{0}x{1}'.format(HEIGHT * UNIT, HEIGHT * UNIT))
         self.shapes = self.load_images()
         self.canvas = self._build_canvas()
         self.texts = []
 
     def _build_canvas(self):
+        """
+        构建用于显示游戏界面的画布。
+
+        该函数创建一个Canvas对象，并在其中绘制游戏所需的网格线和图形。
+        """
+        # 创建画布，设置背景颜色、高度和宽度
         canvas = tk.Canvas(self, bg='white',
                            height=HEIGHT * UNIT,
                            width=WIDTH * UNIT)
-        # create grids
-        for c in range(0, WIDTH * UNIT, UNIT):  # 0~400 by 80
+
+        # 绘制网格线，垂直和水平方向上每隔UNIT距离绘制一条线
+        for c in range(0, WIDTH * UNIT, UNIT):  # 0~400，步长为UNIT
             x0, y0, x1, y1 = c, 0, c, HEIGHT * UNIT
             canvas.create_line(x0, y0, x1, y1)
-        for r in range(0, HEIGHT * UNIT, UNIT):  # 0~400 by 80
+        for r in range(0, HEIGHT * UNIT, UNIT):  # 0~400，步长为UNIT
             x0, y0, x1, y1 = 0, r, HEIGHT * UNIT, r
             canvas.create_line(x0, y0, x1, y1)
 
-        # add img to canvas
+        # 在画布上添加图像，用于表示游戏中的不同形状
         self.rectangle = canvas.create_image(50, 50, image=self.shapes[0])
         self.triangle1 = canvas.create_image(250, 150, image=self.shapes[1])
         self.triangle2 = canvas.create_image(150, 250, image=self.shapes[1])
+        self.triangle3 = canvas.create_image(250, 350, image=self.shapes[1])
         self.circle = canvas.create_image(250, 250, image=self.shapes[2])
 
-        # pack all
+        # 将画布及其内容显示出来
         canvas.pack()
 
+        # 返回创建的画布对象
         return canvas
 
     def load_images(self):
@@ -84,9 +94,27 @@ class Env(tk.Tk):
                         self.text_value(j, i, round(temp, 2), action)
 
     def coords_to_state(self, coords):
+        """
+        将坐标转换为状态编号。
+
+        该方法接收一个坐标的列表，将坐标转换为状态的编号。
+        坐标的转换通过将 x 和 y 轴上的值减去一个偏移量后，除以一个固定的比例来实现。
+        这种转换使得我们可以将连续的坐标空间离散化，便于状态的处理和分析。
+
+        参数:
+        coords: 一个包含两个元素的列表，代表 x 和 y 坐标。
+
+        返回值:
+        一个包含两个整数的列表，代表转换后的状态编号。
+        """
+        # 计算 x 轴上的状态编号
         x = int((coords[0] - 50) / 100)
+        # 计算 y 轴上的状态编号
         y = int((coords[1] - 50) / 100)
+        # 返回转换后的状态编号
+        # 就是把canvas的坐标，转化成物体的数组坐标，比如[50, 50] -> [0, 0]
         return [x, y]
+
 
     def state_to_coords(self, state):
         x = int(state[0] * 100 + 50)
@@ -94,50 +122,88 @@ class Env(tk.Tk):
         return [x, y]
 
     def reset(self):
+        """
+        重置环境到初始状态。
+
+        此函数首先更新环境状态，然后暂停0.5秒，接着将矩形物体移动到画布的中心位置，
+        最后渲染环境并返回当前状态。
+        """
+        # 更新环境状态
         self.update()
+        # 暂停0.5秒，以便观察者可以看到移动过程
         time.sleep(0.5)
+        # 获取矩形物体当前的坐标
         x, y = self.canvas.coords(self.rectangle)
+        # 将矩形物体移动到画布的中心位置
         self.canvas.move(self.rectangle, UNIT / 2 - x, UNIT / 2 - y)
+        # 渲染环境，更新显示
         self.render()
-        # return observation
+        # 返回矩形物体当前的状态，用于下一步操作
         return self.coords_to_state(self.canvas.coords(self.rectangle))
 
     def step(self, action):
+        """
+        执行一个动作并返回结果状态、奖励和是否结束。
+
+        参数:
+        - action: int, 表示采取的动作，0-上，1-下，2-左，3-右。
+
+        返回:
+        - next_state: list, 下一个状态的坐标。
+        - reward: int, 本次动作的得分。
+        - done: bool, 是否游戏结束。
+        """
+        # 获取当前状态（矩形的位置坐标）
         state = self.canvas.coords(self.rectangle)
+        # 初始化基础动作，不移动
         base_action = np.array([0, 0])
+        # 当前结束的话，是否成功
+        successful = 0
+        # 渲染当前画面
         self.render()
 
-        if action == 0:  # up
+        # 根据动作调整位置（绘图）
+        if action == 0:  # 上
             if state[1] > UNIT:
                 base_action[1] -= UNIT
-        elif action == 1:  # down
+        elif action == 1:  # 下
             if state[1] < (HEIGHT - 1) * UNIT:
                 base_action[1] += UNIT
-        elif action == 2:  # left
+        elif action == 2:  # 左
             if state[0] > UNIT:
                 base_action[0] -= UNIT
-        elif action == 3:  # right
+        elif action == 3:  # 右
             if state[0] < (WIDTH - 1) * UNIT:
                 base_action[0] += UNIT
 
-        # 移动
+        # 执行移动
         self.canvas.move(self.rectangle, base_action[0], base_action[1])
+        # 将矩形提升到最上层，确保在其他图形之上
         self.canvas.tag_raise(self.rectangle)
+        # 获取移动后的位置
         next_state = self.canvas.coords(self.rectangle)
-        # 判断得分条件
+
+        # 判断得分情况
         if next_state == self.canvas.coords(self.circle):
+            # 到达目标，游戏结束，得分100
             reward = 100
             done = True
+            successful = 1
         elif next_state in [self.canvas.coords(self.triangle1),
-                            self.canvas.coords(self.triangle2)]:
+                            self.canvas.coords(self.triangle2),
+                            self.canvas.coords(self.triangle3)]:
+            # 碰撞到障碍物，游戏结束，得分-100
             reward = -100
             done = True
         else:
+            # 未到达目标或碰撞，得分为0，游戏继续
             reward = 0
             done = False
 
+        # 将坐标转换为状态
         next_state = self.coords_to_state(next_state)
-        return next_state, reward, done
+        # 返回下一步状态、奖励和游戏是否结束
+        return next_state, reward, done, successful
 
     # 渲染环境
     def render(self):
--- Q-learning/q_learning_agent.py
+++ Q-learning/q_learning_agent.py
@@ -1,3 +1,5 @@
+import pickle
+import os
 import numpy as np
 import random
 from environment import Env
@@ -15,9 +17,24 @@ class QLearningAgent:
 
     # 采样 <s, a, r, s'>
     def learn(self, state, action, reward, next_state):
+        """
+        使用Q-学习算法更新Q表格。
+
+        参数:
+        - state: 当前状态，用于查找当前状态对应的动作-值。
+        - action: 当前采取的动作，用于查找当前状态对应的动作-值。
+        - reward: 从当前状态采取动作后获得的即时奖励。
+        - next_state: 从当前状态采取动作后进入的新状态，用于计算未来最大奖励。
+
+        返回值:
+        无直接返回值，此函数通过更新Q表格来改变对象状态。
+        """
+        # 获取当前状态-动作对应的动作值
         current_q = self.q_table[state][action]
-        # 贝尔曼方程更新
+        # 根据贝尔曼方程更新Q值
+        # 通过当前奖励和未来可能的最大奖励计算新的Q值
         new_q = reward + self.discount_factor * max(self.q_table[next_state])
+        # 更新Q表格，使当前状态-动作的Q值更加接近真实值
         self.q_table[state][action] += self.learning_rate * (new_q - current_q)
 
     # 从Q-table中选取动作
@@ -48,17 +65,42 @@ class QLearningAgent:
 if __name__ == "__main__":
     env = Env()
     agent = QLearningAgent(actions=list(range(env.n_actions)))
-    for episode in range(1000):
+
+    success_lst = []
+
+    # 如果q表文件存在，则加载q表
+    if os.path.exists('./q_table.pkl'):
+        with open('./q_table.pkl', 'rb') as f:
+            agent.q_table = pickle.load(f)
+    for episode in range(100):
+        print('episode: ', episode)
         state = env.reset()
         while True:
             env.render()
             # agent产生动作
             action = agent.get_action(str(state))
-            next_state, reward, done = env.step(action)
+            next_state, reward, done, successful = env.step(action)
+            # 走一步之后，才能开始学习
             # 更新Q表
             agent.learn(str(state), action, reward, str(next_state))
             state = next_state
+
+            # 在单元格上输出Q表
             env.print_value_all(agent.q_table)
             # 当到达终点就终止游戏开始新一轮训练
             if done:
+                success_lst.append(successful)
                 break
+
+    # 保存q表
+    with open('./q_table.pkl', 'wb') as f:
+        pickle.dump(dict(agent.q_table), f)
+
+    # 绘制成功率曲线
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    x = np.arange(0, len(success_lst))
+    y = np.array(success_lst).cumsum()
+    plt.plot(x, y)
+    plt.savefig('./success_rate.png')
